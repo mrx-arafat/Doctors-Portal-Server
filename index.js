691ea8aa4138732bf9ea8a42b6f1dd85c0669200
Easin Arafat <arafatmrx@gmail.com>
2021-11-08 15:32:17 +0600

    basic steps done

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient } = require("mongodb");
 
+const cors = require("cors");
+
+require("dotenv").config();
+
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log(uri);
 app.get("/", (req, res) => {
   res.send("Hello Doctors Portal");
 });
