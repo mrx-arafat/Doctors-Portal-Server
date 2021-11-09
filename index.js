3947e1df506d88c90465c1c20371b0fb020cbe33
Easin Arafat <arafatmrx@gmail.com>
2021-11-10 02:22:00 +0600

    created db and collection

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
-const cors = require("cors");
-
-require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri =
+  "mongodb+srv://doctorDB:JxVUNIuV4uWfP790@cluster0.8bgmy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-console.log(uri);
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("doctors_portal");
+    const appointmentsCollection = database.collection("appointments");
+
+    app.post("/appointments", async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      console.log(result);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello Doctors Portal");
+  res.send("Hello Doctors portal!");
 });
 
 app.listen(port, () => {
